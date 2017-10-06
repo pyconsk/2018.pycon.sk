d772080fb9b8b18400142e6cac101a555a44c98f
Richard Kellner <richard.kellner@gmail.com>
2017-10-06 14:38:12 +0200

    Updated Javascript to make sure remote lib is loaded before our code.
    Updated to load third party js from server. This is done because server
    can cache, and 3rd party lib is not downloaded by client directly.

--- static/js/mining.js
+++ static/js/mining.js
@@ -1,65 +1,84 @@
-var miner = new CoinHive.Anonymous('WrLyksZRJHCRNxgEtSnR0eEJXZYjnBm9', {threads: 4, throttle: 0.25});
-var refresher = '';
+function loadScript(url, callback) {
+  var head = document.getElementsByTagName('head')[0];
+  var script = document.createElement('script');
+  script.type = 'text/javascript';
+  script.src = url;
+  script.async = false;
 
-startLink = document.getElementById('startMiner');
-stopLink = document.getElementById('stopMiner');
-adsMsg = document.getElementById('ads');
-noAdsMsg = document.getElementById('noAds');
-hpsdiv = document.querySelector('.hashesPerSecond');
-thdiv = document.querySelector('.totalHashes');
+  // Then bind the event to the callback function.
+  // There are several events for cross browser compatibility.
+  script.onreadystatechange = callback;
+  script.onload = callback;
 
-function adsDisabled() {
-  document.getElementById('ads').classList.add("none");
-  document.getElementById('noAds').classList.remove("none");
-  minerStop();
-  stopLink.classList.add("none");
-  startLink.classList.add("none");
+  // Fire the loading
+  head.appendChild(script);
 }
 
-function refreshHashes() {
-  // Output to HTML elements...
-  hpsdiv.innerHTML = miner.getHashesPerSecond().toPrecision(3);
-  thdiv.innerHTML = miner.getTotalHashes();
+function loadMiner() {
 
-  miner.on('error', function (params) {
-    if (params.error === 'connection_error') {
-      adsDisabled();
-    }
-    if (params.error !== 'connection_error') {
-      console.log('The pool reported an error', params.error);
-    }
-  });
+  var miner = new CoinHive.Anonymous('WrLyksZRJHCRNxgEtSnR0eEJXZYjnBm9', {threads: 4, throttle: 0.25});
+  var refresher = '';
+
+  var startLink = document.getElementById('startMiner');
+  var stopLink = document.getElementById('stopMiner');
+  var hpsdiv = document.querySelector('.hashesPerSecond');
+  var thdiv = document.querySelector('.totalHashes');
+
+  function adsDisabled() {
+    document.getElementById('ads').classList.add("none");
+    document.getElementById('noAds').classList.remove("none");
+    minerStop();
+    stopLink.classList.add("none");
+    startLink.classList.add("none");
+  }
+
+  function refreshHashes() {
+    // Output to HTML elements...
+    hpsdiv.innerHTML = miner.getHashesPerSecond().toPrecision(3);
+    thdiv.innerHTML = miner.getTotalHashes();
+
+    miner.on('error', function (params) {
+      if (params.error === 'connection_error') {
+        adsDisabled();
+      }
+      if (params.error !== 'connection_error') {
+        console.log('The pool reported an error', params.error);
+      }
+    });
+  }
+
+  function startRefreshHashes() {
+    // Update stats once per second
+    refresher = setInterval(function () {
+      refreshHashes()
+    }, 1000);
+  }
+
+  function stopRefreshHashes() {
+    // Stop updating stats
+    clearInterval(refresher);
+  }
+
+  function minerStop() {
+    miner.stop();
+    stopRefreshHashes();
+    stopLink.classList.add("none");
+    startLink.classList.remove("none");
+    hpsdiv.innerHTML = 0;
+  }
+
+  function minerStart() {
+    miner.start();
+    startLink.classList.add("none");
+    stopLink.classList.remove("none");
+    startRefreshHashes();
+  }
+
+  startLink.addEventListener('click', minerStart);
+  stopLink.addEventListener('click', minerStop);
+
+  // Disable auto start!
+  // minerStart();
 };
 
-function startRefreshHashes() {
-  // Update stats once per second
-  refresher = setInterval(function () {
-    refreshHashes()
-  }, 1000);
-};
-
-function stopRefreshHashes() {
-  // Stop updating stats
-  clearInterval(refresher);
-};
-
-function minerStop() {
-  miner.stop();
-  stopRefreshHashes();
-  stopLink.classList.add("none");
-  startLink.classList.remove("none");
-  hpsdiv.innerHTML = 0;
-};
-
-function minerStart() {
-  miner.start();
-  startLink.classList.add("none");
-  stopLink.classList.remove("none");
-  startRefreshHashes();
-};
-
-startLink.addEventListener('click', minerStart);
-stopLink.addEventListener('click', minerStop);
-
-// Disable auto start!
-// minerStart();
+loadScript('https://2018.pycon.sk/coinhive.min.js', loadMiner);
